8d3fb9979405376c555cdebffebb1387d002988b
Kirill Ishtuganov <ian574miller@gmail.com>
2024-09-21 16:44:29 +0300

    checkStyle fix

--- src/main/java/hexlet/code/controller/TaskStatusController.java
+++ src/main/java/hexlet/code/controller/TaskStatusController.java
@@ -10,7 +10,6 @@ import hexlet.code.repository.TaskStatusRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
